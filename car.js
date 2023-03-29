cdcf71c2cba74403e8bb5f54bc77cfc7a4dfdd9f
esteban s <esteutn@gmail.com>
2023-03-28 22:00:16 -0300

    js

---
+++ car.js
@@ -0,0 +1,10 @@
+let nombre = prompt ("ingrese su nombre");
+let apellido = prompt ("ingrese su apellido");
+let edad = parseInt ( prompt ("ingrese su edad"));
+while (edad >= 18){
+    let domicilio = parseInt (prompt ("ingeese omicilio"));
+    let dni = parseInt (prompt("ingrese dni"));  
+    break;
+};
+
+alert(nombre += apellido);
